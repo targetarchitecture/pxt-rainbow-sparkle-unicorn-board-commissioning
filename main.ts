6e183ccd2c06dd06893ba74981d3e42ea6b988b2
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-12-04 23:36:29 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,28 @@
+RainbowSparkleUnicorn.Switch.onAnyPressed(function (pin) {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
+input.onButtonPressed(Button.A, function () {
+    RainbowSparkleUnicorn.Light.turnAllOn()
+})
 RainbowSparkleUnicorn.Touch.onAnyTouched(function (pin) {
     music.playTone(Math.map(pin, 0, 11, 133, 988), music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    RainbowSparkleUnicorn.Light.turnAllOff()
 })
 RainbowSparkleUnicorn.start()
 RainbowSparkleUnicorn.Light.turnAllOn()
 RainbowSparkleUnicorn.Sound.setVolume(11)
 RainbowSparkleUnicorn.Sound.playTrack(20)
-for (let servos = 0; servos <= 15; servos++) {
-    RainbowSparkleUnicorn.Movement.moveServoBouncy(servos, 0, 180, 20)
-}
-RainbowSparkleUnicorn.Light.turnAllOff()
-for (let lights = 0; lights <= 15; lights++) {
-    RainbowSparkleUnicorn.Light.breathe(
-    lights,
-    1000,
-    1000,
-    500,
-    500
-    )
-}
+RainbowSparkleUnicorn.Movement.moveServoBouncy(RainbowSparkleUnicorn.Movement.Pins.P8, 0, 180, 20)
+RainbowSparkleUnicorn.Movement.moveServoBouncy(RainbowSparkleUnicorn.Movement.Pins.P15, 0, 180, 20)
+// RainbowSparkleUnicorn.Light.breathe(
+// RainbowSparkleUnicorn.Light.Pins.P11,
+// 1000,
+// 1000,
+// 500,
+// 500
+// )
 basic.forever(function () {
     RainbowSparkleUnicorn.Dial.value(RainbowSparkleUnicorn.Dial.Dials.Dial1, randint(0, 255))
     RainbowSparkleUnicorn.Dial.value(RainbowSparkleUnicorn.Dial.Dials.Dial2, randint(0, 255))
